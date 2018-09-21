c5201546c09301afa4f7523f6950f40e7c1c6b0b
tonomony <tonomony@icloud.com>
2018-09-21 12:55:26 +0200

    adding history (not finished)

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Board from './Board';
 import './Game.css';
-import {getNextPlayer, calculateWinner} from '../handlers/GameRules';
+import { getNextPlayer, calculateWinner } from '../handlers/GameRules';
 
 class Game extends React.Component {
   constructor(props) {
@@ -38,6 +38,15 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -55,7 +64,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
