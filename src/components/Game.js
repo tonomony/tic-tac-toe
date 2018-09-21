d36e71ed99cee7f730acc965610f3643f3153015
tonomony <tonomony@icloud.com>
2018-09-21 12:25:05 +0200

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Board from './Board';
+import './Game.css';
+import {getNextPlayer, calculateWinner} from '../handlers/GameRules';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      currentPlayer: 'X'
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    const nextPlayer = getNextPlayer(this.state.currentPlayer);
+    squares[i] = this.state.currentPlayer;
+
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      currentPlayer: nextPlayer
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.currentPlayer);
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
